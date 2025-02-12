f16f855fb4e7a9ad3c38df71f1328242bfbfbfab
Ronald <139284933+Dachina0@users.noreply.github.com>
2025-02-11 21:39:25 -0300

    início do js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const paragrafos = document.querySelectorAll('p');
+console.log(paragrafos);
+
+paragrafos.forEach((item) =>{
+    console.log(item);
+})
+
+paragrafos.forEach((item) =>{
+    console.log(item.innerText);
+})
