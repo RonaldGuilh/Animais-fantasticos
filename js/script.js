e7b7ce5401fd8aee4ac5583c8632d2c46de72a39
Ronald <139284933+Dachina0@users.noreply.github.com>
2025-02-13 13:47:12 -0300

    initTabNav

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-const paragrafos = document.querySelectorAll('p');
-console.log(paragrafos);
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  tabContent[0].classList.add("ativo");
 
-paragrafos.forEach((item) =>{
-    console.log(item);
-})
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
 
-paragrafos.forEach((item) =>{
-    console.log(item.innerText);
-})
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
